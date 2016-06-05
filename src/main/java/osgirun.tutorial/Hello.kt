87eb9f590692ebcfb82a54cc06f5ac6b6da136bd
renato <renato@twobo.com>
2016-06-05 14:11:00 +0200

    Use Frege as a REPL.

--- src/main/java/osgirun.tutorial/Hello.kt
+++ src/main/java/osgirun.tutorial/Hello.kt
@@ -2,13 +2,36 @@ package osgirun.tutorial
 
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
+import java.util.*
+import javax.script.ScriptEngineManager
 
 @Component(immediate = true)
 class Hello {
 
     @Activate
     fun start() {
-        println("Hello osgi-run ${listOf('k', 'o', 't', 'l', 'i', 'n')}")
+        val prompt = "Frege > "
+        val engine = ScriptEngineManager().getEngineByName("frege") ?:
+                throw RuntimeException("Frege ScriptEngine was not found")
+
+        // never block the Component's @Activate method
+        Thread().run {
+            val scanner = Scanner(System.`in`)
+
+            val hasInput = {
+                print(prompt)
+                scanner.hasNextLine()
+            }
+
+            while (hasInput()) {
+                val line = scanner.nextLine()
+                if (line == "exit") {
+                    break
+                } else {
+                    println(engine.eval(line) ?: "")
+                }
+            }
+        }
     }
 
 }
