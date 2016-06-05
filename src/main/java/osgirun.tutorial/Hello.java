4512b2ccff861155b1b8ae6d4a5a18aee036985e
renato <renato@twobo.com>
2016-06-05 19:27:27 +0200

    Let user choose location for weather API

--- src/main/java/osgirun.tutorial/Hello.java
+++ src/main/java/osgirun.tutorial/Hello.java
@@ -7,20 +7,25 @@ import com.github.fedy2.weather.data.unit.DegreeUnit;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
+import java.util.Scanner;
+
 @Component( immediate = true )
 public class Hello {
 
     @Activate
     public void start() {
+        System.out.println( "Enter a location:" );
+        String location = new Scanner( System.in ).nextLine();
+
         try {
             YahooWeatherService service = new YahooWeatherService();
-            Channel channel = service.getForecastForLocation( "Stockholm, Sweden", DegreeUnit.CELSIUS )
+            Channel channel = service.getForecastForLocation( location, DegreeUnit.CELSIUS )
                     .first( 1 ).get( 0 );
 
             int temperature = channel.getItem().getCondition().getTemp();
             String conditions = channel.getItem().getCondition().getText();
 
-            System.out.println( "Weather in Stockholm now: " +
+            System.out.println( "Weather in " + location + " now: " +
                     temperature + "C, " + conditions );
         } catch ( Exception e ) {
             e.printStackTrace();
